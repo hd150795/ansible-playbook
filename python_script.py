c44ee48a17ee651c5beee92d6800c0eb21769680
GGK_CENTOS <GGKU3SYS247@GGKU3SYS247.ggktech.local>
2016-10-24 17:19:12 +0530

    deploy

--- python_script.py
+++ python_script.py
@@ -8,12 +8,13 @@ reservations=connection.get_all_instances();
 for res in reservations:
   for inst in res.instances:
    if 'travis' in inst.tags:
-    print "%s" % (inst.ip_address)
+    if inst.state == "running":
+     print "%s" % (inst.ip_address)
 
-    with open("inventory", "r") as in_file:
+     with open("inventory", "r") as in_file:
       buf = in_file.readlines()
 
-    with open("inventory", "w") as out_file:
+     with open("inventory", "w") as out_file:
       for line in buf:
         out_file.write(line)
         if '[webservers]' in line:
